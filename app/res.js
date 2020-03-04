036ddeb60dcb315c9f1890523a4bdb11621b537c
Aliaksandr Litskevich <alitskevich@gmail.com>
2020-03-04 17:14:40 +0300

    sample app working

--- app/res.js
+++ app/res.js
@@ -2,8 +2,8 @@ export default {
     top: {
         title: 'The Sample',
         sitemap: [
-            { id: 'main', name: 'Main' },
-            { id: 'todo', name: 'To-Do' },
+            { id: 'main', name: 'Main', icon2:'home' },
+            { id: 'todo', name: 'To-Do', icon2:'work' },
         ],
     }
 }
